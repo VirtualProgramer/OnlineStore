afaa9933cc010dfb2f2814bf27e5e95b7a1fb1cf
Bennnn123 <p33p33797@gmail.com>
2019-06-10 22:44:54 +0800

    新增加購物車網頁及調整產品列表需登入才能瀏覽

--- wsqwe/ProductView.aspx.cs
+++ wsqwe/ProductView.aspx.cs
@@ -7,6 +7,10 @@ using System.Web.UI.WebControls;
 
 public partial class ProductView : System.Web.UI.Page {
     protected void Page_Load(object sender, EventArgs e) {
+        if (Session["UserName"] == null)
+        {
+            Response.Redirect("~/Login.aspx");
+        }
 
     }
 
